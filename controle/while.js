7c3dbf1374098124357fc4f30132836d6422cbe1
arturgomesc <arturgomesc@hotmail.com>
2022-11-16 16:54:19 -0300

    novos commits

---
+++ controle/while.js
@@ -0,0 +1,16 @@
+// while: estrutura para numero indeterminado de repeticoes
+
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min;
+    return Math.floor(valor)
+
+    }
+
+    let opcao = 0
+
+    while(opcao != -1) {
+        opcao = getInteiroAleatorioEntre(-1, 10)
+        console.log(`Opcao escolhida foi ${opcao}.`);
+    }
+
+    console.log('Ate a proxima!');
